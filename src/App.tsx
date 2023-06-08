84ba7e43d240905dcc4086098a89071c8692a806
pedro.luiz <pedro.aquino@liax.tech>
2023-06-08 14:23:58 -0300

    color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
+import { ThemeProvider } from '@emotion/react';
+import { LightTheme } from './shared/themes';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
