44cee36af76261848fbaf8e15229243081513878
pedro.luiz <pedro.aquino@liax.tech>
2023-06-14 08:14:57 -0300

    giut pull navbar resolve padding fix

--- src/shared/layouts/Navbar.tsx
+++ src/shared/layouts/Navbar.tsx
@@ -3,7 +3,7 @@ import { MdDensityMedium, MdSearch } from 'react-icons/md';
 import { useDrawerContext } from '../contexts';
 import logo from './logo_sellhardware.png';
 import { useNavigate } from 'react-router-dom';
-import { useTheme, TextField, Button, Paper, InputAdornment, Icon, InputBase, IconButton } from '@mui/material';
+import { useTheme, TextField, Button, Paper, InputAdornment, Icon, InputBase, IconButton, useMediaQuery, Avatar } from '@mui/material';
 import { useState } from 'react';
 
 
@@ -15,13 +15,16 @@ export const Navbar: React.FC<INavebar> = ({ children }) => {
     const { toggleDrawerOpen } = useDrawerContext();
     const theme = useTheme();
     const navigate = useNavigate();
+    const mdDown = useMediaQuery(theme.breakpoints.down('md'));
+    const lgDown = useMediaQuery(theme.breakpoints.down('lg'));
     const handleClickSearch = () => {
     }
     return (
         <>
+
             <Box
                 bgcolor={"#465EFF"}
-                width={"100%"}
+                width={"100vw"}
                 height={theme.spacing(8)}
                 sx={{ zIndex: 1 }}
                 position={'fixed'}
@@ -31,23 +34,27 @@ export const Navbar: React.FC<INavebar> = ({ children }) => {
                 borderRadius={0}
                 padding={1}
                 paddingX={1}
+                justifyContent={'space-between'}
                 component={Paper}
-                elevation={2}
+                elevation={5}
             >
-                <Box component={Button} >
-                    <MdDensityMedium color='#ffffff' size={'2.5rem'} onClick={toggleDrawerOpen} style={{ cursor: "pointer" }} />
-                </Box>
-                <Box component={Button} onClick={handleClickSearch}>
-                    <img src={logo} width={"50px"} alt='Logo Sell Hardware' />
+                <Box display={'flex'}>
+                    <Box component={Button} display={'flex'}>
+                        <MdDensityMedium color='#ffffff' size={'2.5rem'} onClick={toggleDrawerOpen} style={{ cursor: "pointer" }} />
+                    </Box>
+                    <Box component={Button} onClick={handleClickSearch} display={'flex'}>
+                        <img src={logo} width={"50px"} alt='Logo Sell Hardware' />
+                    </Box>
                 </Box>
                 <Box
                     padding={1}
                     paddingY={2}
                     margin={1}
+                    width={'20%'}
                     justifyContent={'end'}
                     display={'flex'}
                     alignItems={'center'}
-                    style={{ backgroundColor: '#ffffff', borderRadius: '4px' }}
+                    style={{ backgroundColor: '#ffffff', borderRadius: '4px', justifyContent: 'center' }}
                 >
                     <InputBase
                         sx={{ ml: 1, flex: 1 }}
@@ -57,6 +64,18 @@ export const Navbar: React.FC<INavebar> = ({ children }) => {
                     <IconButton type="button" sx={{ p: '10px' }} aria-label="Pesquisar">
                         <MdSearch />
                     </IconButton>
+                </Box>
+                <Box
+                    padding={1}
+                    paddingY={2}
+                    margin={1}
+                    justifyContent={'end'}
+                    display={'flex'}
+                    alignItems={'center'}
+                >
+                    <Avatar>
+
+                    </Avatar>
                 </Box>
             </Box>
             <Box>
